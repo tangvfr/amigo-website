2ee922b2d83220095c5d7b9b251672ed75664b2b
Baboux <baptiste.roquin@etu.univ-orleans.com>
2024-03-04 22:55:02 +0100

    tout les crud fait, rien de special

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Company;
+use App\Entity\Event;
+use App\Entity\Location;
+use App\Entity\Mandate;
+use App\Entity\Offer;
+use App\Entity\Partner;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -27,5 +32,10 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Entreprise', 'fas fa-building', Company::class);
+        yield MenuItem::linkToCrud('Evenement', 'fas fa-calendar', Event::class);
+        yield MenuItem::linkToCrud('Localisation', 'fas fa-map-location-dot', Location::class);
+        yield MenuItem::linkToCrud('Mandate', 'fas fa-person', Mandate::class);
+        yield MenuItem::linkToCrud('Offre', 'fas fa-user-tie', Offer::class);
+        yield MenuItem::linkToCrud('Partenaire', 'fas fa-handshake', Partner::class);
     }
 }
--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -2,36 +2,37 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Company;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use App\Entity\Event;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
-
-class CompanyCrudController extends AbstractCrudController
+class EventCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return Company::class;
+        return Event::class;
     }
 
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
-            ->setEntityLabelInSingular('Company')
-            ->setEntityLabelInPlural('Company')
+            ->setEntityLabelInSingular('Event')
+            ->setEntityLabelInPlural('Events')
             ->setSearchFields(['name'])
             ->setDefaultSort(['id' => 'DESC']);
     }
 
+    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('name'),
-            TextField::new('img'),
-            TextEditorField::new('banner'),
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
         ];
     }
+    */
 }
