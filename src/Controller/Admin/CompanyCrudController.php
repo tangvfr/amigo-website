cac69d0f26a03241dae903c00d1eac3b142f36a6
Baboux <baptiste.roquin@etu.univ-orleans.com>
2024-03-04 18:13:57 +0100

    CRUD de company dans le dashboard easy admin

---
+++ src/Controller/Admin/CompanyCrudController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Company;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class CompanyCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Company::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Company')
+            ->setEntityLabelInPlural('Company')
+            ->setSearchFields(['name'])
+            ->setDefaultSort(['id' => 'DESC']);
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
