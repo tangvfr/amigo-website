6e3585e15bede115d56fee5404f511acd61b4cc2
Baboux <baptiste.roquin@etu.univ-orleans.com>
2024-03-05 12:41:48 +0100

    deplacement de tout le projet dans core pour facilité le merge

---
+++ core/src/Controller/Admin/EventCrudController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Event;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+
+class EventCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Event::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Evenement')
+            ->setEntityLabelInPlural('Evenements')
+            ->setSearchFields(['name'])
+            ->setDefaultSort(['id' => 'DESC'])
+            ->setDateFormat('dd/MM/yyyy')
+            ->setPageTitle('index', 'Amigo Website - Event');
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
