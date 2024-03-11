1929c92c8ffc57ab3a6d8d28ac71ac81927fe684
Baboux <baptiste.roquin@etu.univ-orleans.com>
2024-03-11 12:47:40 +0100

    Dans Company les fichier qui sont upload ne peuvent etre que des jpeg/png/gif d'une taille de 1024k

--- core/src/Controller/Admin/CompanyCrudController.php
+++ core/src/Controller/Admin/CompanyCrudController.php
@@ -10,10 +10,22 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Image;
 
 class CompanyCrudController extends AbstractCrudController
 {
+
+    public function isImage(?string $img): bool
+    {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type aka mimetype extension
+
+        $type_file = finfo_file($finfo, $img);
+
+        finfo_close($finfo);
+        return ($type_file == 'image/jpeg' || $type_file == 'image/png' || $type_file == 'image/gif');
+    }
+
     public static function getEntityFqcn(): string
     {
         return Company::class;
@@ -40,13 +52,51 @@ class CompanyCrudController extends AbstractCrudController
                 ->setUploadDir('public/uploads/')
                 ->setUploadedFileNamePattern('[randomhash].[extension]')
                 ->setRequired(false)
-                ->setSortable(false),
+                ->setSortable(false)
+                ->setFormTypeOptions([
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '1024k',
+                            'mimeTypes' => [
+                                'image/jpeg',
+                                'image/png',
+                                'image/gif',
+                            ],
+                            'mimeTypesMessage' => 'Veuillez télécharger une image au format JPEG, PNG ou GIF',
+                        ]),
+                        // new Image([
+                        //     'maxWidth' => 1200,
+                        //     'maxHeight' => 1200,
+                        //     'maxWidthMessage' => 'La largeur de l\'image ne doit pas dépasser 1200 pixels',
+                        //     'maxHeightMessage' => 'La hauteur de l\'image ne doit pas dépasser 1200 pixels',
+                        // ]),
+                    ],
+                ]),
             ImageField::new('banner', 'Bannière')
                 ->setBasePath('uploads/')
                 ->setUploadDir('public/uploads/')
                 ->setUploadedFileNamePattern('[randomhash].[extension]')
                 ->setRequired(false)
-                ->setSortable(false),
+                ->setSortable(false)
+                ->setFormTypeOptions([
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '1024k',
+                            'mimeTypes' => [
+                                'image/jpeg',
+                                'image/png',
+                                'image/gif',
+                            ],
+                            'mimeTypesMessage' => 'Veuillez télécharger une image au format JPEG, PNG ou GIF',
+                        ]),
+                        // new Image([
+                        //     'maxWidth' => 1200,
+                        //     'maxHeight' => 1200,
+                        //     'maxWidthMessage' => 'La largeur de l\'image ne doit pas dépasser 1200 pixels',
+                        //     'maxHeightMessage' => 'La hauteur de l\'image ne doit pas dépasser 1200 pixels',
+                        // ]),
+                    ],
+                ]),
             TextEditorField::new('description', 'Description')
                 ->hideOnIndex(),
             AssociationField::new('located', 'Emplacements'),
@@ -65,7 +115,6 @@ class CompanyCrudController extends AbstractCrudController
                 $images[] = $entityInstance->getImg();
             if ($entityInstance->getBanner() != null)
                 $images[] = $entityInstance->getBanner();
-
             // Supprime les images physiques du système de fichiers
             foreach ($images as $image) {
                 $filePath = $this->getParameter('kernel.project_dir') . '/public/uploads/' . $image;
@@ -77,5 +126,9 @@ class CompanyCrudController extends AbstractCrudController
 
         // Appel de la méthode parente pour effectuer la suppression de l'entité
         parent::deleteEntity($entityManager, $entityInstance);
+    }
+
+    public function createEntity(string $entityFqcn)
+    {
     }
 }
