a602496a6c7c345476e85deae9d8c99fb323bb4d
Baboux <baptiste.roquin@etu.univ-orleans.com>
2024-03-13 00:02:04 +0100

    debut de l'affichage correct des elements

--- core/src/Controller/Admin/EventCrudController.php
+++ core/src/Controller/Admin/EventCrudController.php
@@ -5,11 +5,19 @@ namespace App\Controller\Admin;
 use App\Entity\Event;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
-class EventCrudController extends AbstractCrudController
+class EventCrudController extends AbstractImageCrudController
 {
     public static function getEntityFqcn(): string
     {
@@ -27,14 +35,47 @@ class EventCrudController extends AbstractCrudController
             ->setPageTitle('index', 'Amigo Website - Event');
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
+        // 14 écrit dans la classe
+        // 13
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->hideOnIndex()
+                ->setFormTypeOptions(['disable']),
+            TextField::new('name', 'Nom'),
+            ImageField::new('img', 'Image')
+                ->setBasePath(self::BASE_PATH)
+                ->setUploadDir(self::UPLOAD_DIR),
+            TextEditorField::new('description', 'Description'),
+            BooleanField::new('onlyMiagist', 'Réserver aux miagiste'),
+            ChoiceField::new('note')
+                ->setChoices([1,2,3,4,5]),
+
+            // ARGENT
+            MoneyField::new('nadhPrice', 'Prix Non Adhérant')
+                ->setCurrency('EUR'),
+            MoneyField::new('adhPrice', 'Prix Adhérant')
+                ->setCurrency('EUR'),
+
+            // QUOTA
+            IntegerField::new('quotaStu', 'Quota d\'éleve'),
+            IntegerField::new('quotaComp', 'Quota complet'),
+
+
+            AssociationField::new('types'),
+            AssociationField::new('situated', 'Localisation de l\'event'),
+
+            // DATE
+            DateTimeField::new('bgedDate', 'Date de début')
+                ->setFormTypeOptions(['disable'])
+                ->renderAsNativeWidget(false),
+
+
+            BooleanField::new('cancel', 'Annuler'),
+
         ];
     }
-    */
+
 }
