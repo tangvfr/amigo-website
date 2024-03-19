22e0895041bf3a5a4cc3ea281074c309a0d6ad93
tangvfr <tangv.minecraft@gmail.com>
2024-03-19 08:39:04 +0000

    Ajout classe metier pour création et gestion de different position/location

--- frontend/src/app/models/map/location.ts
+++ frontend/src/app/models/map/location.ts
@@ -2,8 +2,16 @@ export interface Location {
   label: string;
   latitude: number;
   longitude: number;
-  country: string;
-  city: string;
-  postalCode: string;
-  adresse: string;
+  country?: string;
+  city?: string;
+  postalCode?: string;
+  adresse?: string;
+}
+
+export function inlineAddressLoc(loc: Location) {
+  return (loc.adresse !== undefined ? loc.adresse + ', ' : '')
+    + (loc.country ?? '')
+    + ' ' + (loc.city ?? '')
+    + ' ' + (loc.country ?? '')
+  ;
 }
