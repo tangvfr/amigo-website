eb5248ee813897d3986c37c31088a014d2dcfc08
Elpapelito <d.redguns45@gmail.com>
2024-03-20 16:00:38 +0000

    patch constantes dashboardController

--- core/src/Controller/Admin/ConstantesCrud.php
+++ core/src/Controller/Admin/ConstantesCrud.php
@@ -4,16 +4,41 @@ namespace App\Controller\Admin;
 
 abstract class ConstantesCrud {
 
-    //contantes générales
+    //contantes générales + entités
     const SITE_NAME = 'Amigo Website';
     const DASHBOARD_NAME = 'Tableau de bord';
-    const DASHBOARD_ICON = 'fa fa-home';
-
     const EXPOSED_NAME = 'Données exposées';
-    const EXPOSED_ICON = 'fa-solid fa-signs-post';
-
     const OFFICE_NAME = 'Bureau';
+    const COMPANY_NAME = 'Entreprise';
+    const COMPANY_TYPE_NAME = 'Entreprise Type';
+    const EVENT_NAME = 'Évènement';
+    const EVENT_TYPE_NAME = 'Évènement Type';
+    const HUB_NAME = 'Pole';
+    const LOCATION_NAME = 'Localisation';
+    const MANDATE_NAME = 'Mandat';
+    const OFFER_NAME = 'Offre';
+    const PARTNER_NAME = 'Partenaire';
+    const ROLE_NAME = 'Role';
+    const STUDENT_NAME = 'Étudiant';
+
+
+    //icons
+    const DASHBOARD_ICON = 'fa fa-home';
+    const EXPOSED_ICON = 'fa-solid fa-signs-post';
     const OFFICE_ICON = 'fa-solid fa-briefcase';
+    const PASSWORD_ICON = 'fa-solid fa-key';
+    const COMPANY_ICON = 'fas fa-building';
+    const COMPANY_TYPE_ICON = 'fas fa-chart-line';
+    const EVENT_ICON = 'fas fa-calendar';
+    const EVENT_TYPE_ICON = 'fas fa-champagne-glasses';
+    const LOCATION_ICON = 'fas fa-map-location-dot';
+    const OFFER_ICON = 'fas fa-user-tie';
+    const PARTNER_ICON = 'fas fa-handshake';
+    const STUDENT_ICON = 'fas fa-user';
+    const MANDATE_ICON = 'fas fa-person';
+    const HUB_ICON = 'fas fa-flag';
+    const ROLE_ICON = 'fas fa-key';
+
 
 
     // Taille des panels
@@ -33,6 +58,5 @@ abstract class ConstantesCrud {
     const PANEL_NAME_QUOTAS = 'QUOTAS';
     const PANEL_NAME_INFOS = 'INFOS';
     const PASSWORD_NAME = 'Changer de mot de passe';
-    const PASSWORD_ICON = 'fa-solid fa-key';
 
 }
