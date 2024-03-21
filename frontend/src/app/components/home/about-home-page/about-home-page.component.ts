38451926702cecc0a26a45a7272816513f2e931b
tangvfr <tangv.minecraft@gmail.com>
2024-03-21 14:30:49 +0000

    modif visuel

--- frontend/src/app/components/home/about-home-page/about-home-page.component.ts
+++ frontend/src/app/components/home/about-home-page/about-home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-about-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './about-home-page.component.html',
   styleUrl: './about-home-page.component.css'
 })
